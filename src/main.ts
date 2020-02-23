9dad2a9212c58e3d4a308734d3e32f6a1a12c96a
Webkunx <stormvanya@gmail.com>
2020-02-23 20:48:36 +0200

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import * as dotenv from "dotenv";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "@nestjs/common";
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle("Giphy API")
+    .setDescription("My small api for GIPHY interactions")
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup("api", app, document);
   await app.useGlobalPipes(new ValidationPipe()).listen(3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
 import { IsNotEmpty, Min, Max, IsEmail } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   username: string;
 }
